eac25dfe40b435760deb7319c2a18ec2a7213b6b
NorieLiu <111670625+NorieLiu@users.noreply.github.com>
2022-08-21 13:16:59 +1000

    created my temperature sensor code.

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.temperature()))
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
-	
+    if (input.temperature() >= 20) {
+        music.playMelody("C E D F E G F A ", 120)
+    } else if (input.temperature() < 20) {
+        music.playMelody("C5 A B G A F G E ", 120)
+    }
 })
